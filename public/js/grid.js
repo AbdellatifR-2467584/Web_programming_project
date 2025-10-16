8038ccdf4b74f85cabeb9f22c717ef4106613146
Brent <brent.franco@student.uhasselt.be>
2025-10-16 17:46:07 +0200

    holyyyy

--- public/js/grid.js
+++ public/js/grid.js
@@ -1,43 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const homeButton = document.querySelector(".bi.bi-house");
-    const uploadButton = document.querySelector(".bi.bi-upload");
-    const uploadLinkButton = document.querySelector(".bi.bi-link-45deg");
-
-    homeButton.addEventListener("click", () => {
-        window.location.href = "/";
-    });
-
-    uploadButton.addEventListener("click", () => {
-        window.location.href = "/upload";
-    });
-
-    uploadLinkButton.addEventListener("click", () => {
-        window.location.href = "/uploadlink";
-    });
-
-    const searchForm = document.querySelector(".searchform");
-    const searchInput = document.querySelector(".searchinput");
-
-    searchForm.addEventListener("submit", (event) => {
-        event.preventDefault(); // voorkomt herladen van de pagina
-
-        const query = searchInput.value.trim();
-
-        if (query) {
-            // hier ga je de grid refreshen met zoekresultaat in de database
-
-
-        }
-    });
-});
-
-
 document.addEventListener("DOMContentLoaded", async () => {
     const grids = document.getElementsByClassName("grid");
     const res = await fetch("/api/posts");
     const posts = await res.json();
+    let currentPostId = null;
+    const pathParts = window.location.pathname.split("/").filter(Boolean);
+    if (pathParts[0] === "post" && !isNaN(pathParts[1])) {
+        currentPostId = parseInt(pathParts[1]);
+    }
+
+
     for (const grid of grids) {
-        grid.innerHTML = posts.map(post => `
+        grid.innerHTML = posts
+            .filter(post => post.id !== currentPostId)
+            .map(post => `
         <div class="card" onclick="location.href='/post/${post.id}'">
             <img src="${post.image_path}" alt="Recipe">
         </div>
