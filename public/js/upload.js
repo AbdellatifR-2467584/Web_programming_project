64bbd0de496ba3799bb24b1be72510594d49b543
Brent <brent.franco@student.uhasselt.be>
2025-10-24 18:01:20 +0200

    bijna fix

--- public/js/upload.js
+++ public/js/upload.js
@@ -156,7 +156,7 @@ document.addEventListener("DOMContentLoaded", () => {
     stappenlijst.addEventListener('click', (e) => {
       if (e.target.classList.contains('remove')) {
         e.target.parentElement.remove();
-      } else if (e.target.classList.contains('add')) {
+      } else if (e.target.closest('.add')) {
         addStep();
       }
     });
