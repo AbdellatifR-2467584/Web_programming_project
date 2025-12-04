1f68fdc764eb86a0fc652be128d649457d25adef
Brent <brent.franco@student.uhasselt.be>
2025-12-04 05:15:22 +0100

    oui?

--- public/js/post.js
+++ public/js/post.js
@@ -5,40 +5,11 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const postDiv = document.querySelector(".ingredienten");
-    const ingredients = JSON.parse(postDiv.dataset.ingredients);
-    const ul = document.querySelector(".list-ingredienten");
-    for (const ingredient of ingredients) {
-        if (ingredient != "") {
-            const li = document.createElement("li");
-            li.textContent = ingredient;
-            ul.appendChild(li);
-        }
-    }
-});
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const postDiv = document.querySelector(".instructies");
-    const instructies = JSON.parse(postDiv.dataset.ingredients);
-    const ol = document.querySelector(".list-stappen");
-    for (const instructie of instructies) {
-        if (instructie != "") {
-            const li = document.createElement("li");
-            li.textContent = instructie;
-            ol.appendChild(li);
-        }
-    }
-});
-
-
 function download_pdf() {
     const pdf = document.getElementById("post");
     const title = pdf.dataset.title;
     html2pdf().from(pdf).save(title);
 }
-
 
 async function clipboard_copy() {
     const titel = document.getElementById('postTitel').textContent.trim();
