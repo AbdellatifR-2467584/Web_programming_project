61821726977459bb84b3d570ddc04f5e02967f59
Brent <brent.franco@student.uhasselt.be>
2025-12-15 19:21:15 +0100

    test

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const homeButton = document.querySelector(".bi.bi-house");
     const uploadButton = document.querySelector(".bi.bi-upload");
-    const uploadLinkButton = document.querySelector(".bi.bi-link-45deg");
     const ingredientsButton = document.querySelector(".bi.bi-basket");
 
-    homeButton.addEventListener("click", () => {
-        window.location.href = "/";
-    });
+    if (homeButton) {
+        homeButton.addEventListener("click", () => {
+            window.location.href = "/";
+        });
+    }
 
     uploadButton.addEventListener("click", () => {
         window.location.href = "/upload";
